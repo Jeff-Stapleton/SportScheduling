a13e6ff0c077eb501504e942bf8e31ab92976055
Andrew <wally451@sbcglobal.net>
2014-11-28 21:59:56 -0700

    added sign in page folder w/ all the necessary files

---
+++ sign in page/app.js
@@ -0,0 +1,14 @@
+angular.module('SportsScheduler', [])
+.controller('MainCtrl', [
+	'$scope',
+	function($scope){
+		$scope.test = 'Hello World!';
+	}]);
+
+$scope.posts = [
+	'post 1',
+	'post 2',
+	'post 3',
+	'post 4',
+	'post 5'
+];
