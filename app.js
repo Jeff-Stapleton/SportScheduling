babd02965d53f364872d7b2ebcd2cd93f224b1e1
Jeffrey D Stapleton <stapl3@paintball.cs.byu.edu>
2014-12-04 20:07:22 -0700

    clean up

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
 		this.user = user; 
 	});
 
-	app.controller('FacilityController', function(){
-		this.facility = facility;
+	app.controller('FacilityCtrl', function(){
+		this.facilities = facilities;
 	});
 
 	var user = {
@@ -16,8 +16,53 @@
 
 	var facilites = [
 		
-		var facility = {
-
+		{
+			name: 'Provo Recreation Center',
+			file: 'ProvoRec.jpg',
+			link: '',
+			imageLink: '<img src="img/ProvoRec.jpg" alt="Provo Rec Center" >',
+		}
+		{
+			name: 'Orem Recreation Center',
+			file: 'OremRec.jpg',
+			link: 'http://www.oremrecreation.com/index.php/fitness-center',
+			imageLink: '<img src="img/OremRec.jpg" alt="Orem Rec Center" >',
+		}
+		{
+			name: 'Richards Building - BYU'
+			file: 'rb.jpg',
+			link: 'http://rbfacilities.byu.edu/content/information-center',
+			imageLink: '<img src="img/rb.jpg" alt="Richards Building - BYU" >',
+		}
+		{
+			name: '24 Hour Fitness',
+			file: '24Hour.jpg',
+			link: 'https://www.24hourfitness.com/Website/Club/343',
+			imageLink: '<img src="img/24Hour.jpg" alt="24 Hour Fitness" >',
+		}
+		{
+			name: 'Provo Recreation Center',
+			file: 'ProvoRec.jpg',
+			link: '',
+			imageLink: '<img src="img/ProvoRec.jpg" alt="Provo Rec Center" >',
+		}
+		{
+			name: 'Orem Recreation Center',
+			file: 'OremRec.jpg',
+			link: 'http://www.oremrecreation.com/index.php/fitness-center',
+			imageLink: '<img src="img/OremRec.jpg" alt="Orem Rec Center" >',
+		}
+		{
+			name: 'Richards Building - BYU'
+			file: 'rb.jpg',
+			link: 'http://rbfacilities.byu.edu/content/information-center',
+			imageLink: '<img src="img/rb.jpg" alt="Richards Building - BYU" >',
+		}
+		{
+			name: '24 Hour Fitness',
+			file: '24Hour.jpg',
+			link: 'https://www.24hourfitness.com/Website/Club/343',
+			imageLink: '<img src="img/24Hour.jpg" alt="24 Hour Fitness" >',
 		}
 	]
 
