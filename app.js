43807f409ef6c1ff468bfdc6d151dbc75b43d787
Jeffrey D Stapleton <stapl3@paintball.cs.byu.edu>
2014-12-04 19:52:25 -0700

    style changes

---
+++ app.js
@@ -0,0 +1,41 @@
+(function(){
+	var app = angular.module('home', []);
+
+	app.controller('HomeCtrl', function(){
+		this.user = user; 
+	});
+
+	app.controller('FacilityController', function(){
+		this.facility = facility;
+	});
+
+	var user = {
+
+
+	}
+
+	var facilites = [
+		
+		var facility = {
+
+		}
+	]
+
+})();
+
+(function(){
+	var app = angular.module('SportSceduler', []);
+
+	app.controller('UserCtrl', function(){
+		this.user = user; 
+	});
+
+	var user = {
+		userId: 0,
+		firstName: '',
+		lastName: '',
+		isSignedIn: false,
+	}
+
+
+})();
---
+++ app.js~
@@ -0,0 +1,24 @@
+(function(){
+	var app = angular.module('home', []);
+
+	app.controller('HomeCtrl', function(){
+		this.user = user; 
+	});
+
+	app.controller('FacilityController', function(){
+		this.facility = facility;
+	});
+
+	var user = {
+
+
+	}
+
+	var facilites = [
+		
+		var facility = {
+
+		}
+	]
+
+})();
