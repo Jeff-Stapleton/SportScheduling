aad054ef30df79e786587848807d78c973801089
Andrew <wally451@sbcglobal.net>
2014-12-06 15:29:27 -0700

    updated controllers and variables

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,14 @@
-(function() {
-	var app = angular.module('SportScheduler', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 
-								'NerdService', 'GeekCtrl', 'GeekService']);
+// (function() {
+// 	var app = angular.module('SportScheduler', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 
+// 								'NerdService', 'GeekCtrl', 'GeekService']);
 
-))();
+// ))();
 
 
 
 
 (function(){
-	var app = angular.module('home', []);
+	var app = angular.module('SportScheduler', []);
 
 	app.controller('HomeCtrl', function(){
 		this.user = user; 
@@ -19,76 +19,67 @@
 	});
 
 	var user = {
-
-
+		firstName: 'Jeff',
+		lastName: 'Stapleton',
+		email: 'jeff.d.stapleton@gmail.com',
+		phone: '(949) 632-0102'
 	}
 
 	var facilities = [
 		
 		{
-			name: 'Provo Recreation Center',
-			file: 'ProvoRec.jpg',
-			link: '',
-			images: [
-				"ProvoRec.jpg" 
-			]
-		}
-		{
-			name: 'Orem Recreation Center',
-			file: 'OremRec.jpg',
-			link: 'http://www.oremrecreation.com/index.php/fitness-center',
+			name: 'Richards Building - BYU',
+			hours: '6:00 am - 9:45 pm Mon-Sat',
+			phone: '(801) 422-3644',
 			images: [ 
-				"OremRec.jpg"
-			]
-		}
-		{
-			name: 'Richards Building - BYU'
-			file: 'rb.jpg',
-			link: 'http://rbfacilities.byu.edu/content/information-center',
-			images: [ 
-				"rb.jpg"
+				"../assets/rb.jpg"
 			]
 		}
 		{
 			name: '24 Hour Fitness',
-			file: '24Hour.jpg',
-			link: 'https://www.24hourfitness.com/Website/Club/343',
+			hours: '6:00 am - 12:00 pm Mon-Sat',
+			phone: '(801) 224-2096',
 			images: [
-				"24Hour.jpg"
-			]
-		}
-		{
-			name: 'Provo Recreation Center',
-			file: 'ProvoRec.jpg',
-			link: '',
-			images: [
-				"ProvoRec.jpg" 
+				"../assets/24Hour.jpg"
 			]
 		}
 		{
 			name: 'Orem Recreation Center',
-			file: 'OremRec.jpg',
-			link: 'http://www.oremrecreation.com/index.php/fitness-center',
+			hours: '5:00 am - 10:00 pm Mon-Fri
+					7:00 am - 7:00 pm Sat',
+			phone: '(801) 852-6600',
 			images: [ 
-				"OremRec.jpg"
+				"../assets/OremRec.jpg"
 			]
 		}
 		{
-			name: 'Richards Building - BYU'
-			file: 'rb.jpg',
-			link: 'http://rbfacilities.byu.edu/content/information-center',
-			images: [ 
-				"rb.jpg"
-			]
-		}
-		{
-			name: '24 Hour Fitness',
-			file: '24Hour.jpg',
-			link: 'https://www.24hourfitness.com/Website/Club/343',
+			name: 'Provo Recreation Center',
+			hours: '5:00 am - 10:00 pm Mon-Fri
+					7:00 am - 8:00 pm Sat',
+			phone: '(801) 229-7154',
 			images: [
-				"24Hour.jpg"
+				"../assets/ProvoRec.jpg" 
 			]
 		}
 	]
+
+	var facility = {
+		name: '',
+		hours: '',
+		phone: '',
+		images: [],
+		courts: [
+
+			{
+				name: '',
+				img: '',
+				schedule: [
+					date: '',
+					
+				]
+
+			}
+		],
+	}
 
 })();
