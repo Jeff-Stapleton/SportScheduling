8a779bd66dc37da9342f1a1dfbf7ac7963fc2c3c
Andrew <wally451@sbcglobal.net>
2014-12-08 18:05:27 -0700

    updated angular

--- public/js/app.js
+++ public/js/app.js
@@ -22,26 +22,13 @@
 		}; 
 
 		$scope.submit = function(data) {
-			
+
 		};
 
 	}]);
 
 	app.controller('FacilityController', ['$scope', function($scope){
-		$scope.facilities = facilities;
-	}]);
-
-	// var user = {
-	// 	firstName: 'Jeff',
-	// 	lastName: 'Stapleton',
-	// 	email: 'jeff.d.stapleton@gmail.com',
-	// 	phone: '(949) 632-0102',
-	// 	fbUserId: '',
-	// 	accessToken: '',
-	// 	image: 'http://4.bp.blogspot.com/-xsZFgAwTaow/UumqvTFjURI/AAAAAAAACGI/LdXISuoVc00/s1600/vscocam_1390962236.676736.55.jpg'
-	// }
-
-	var facilities = [
+		$scope.facilities = [
 		
 		{
 			name: 'Richards Building - BYU',
@@ -77,7 +64,56 @@
 				"../assets/ProvoRec.jpg" 
 			]
 		}
-	]
+	];
+	}]);
+
+	// var user = {
+	// 	firstName: 'Jeff',
+	// 	lastName: 'Stapleton',
+	// 	email: 'jeff.d.stapleton@gmail.com',
+	// 	phone: '(949) 632-0102',
+	// 	fbUserId: '',
+	// 	accessToken: '',
+	// 	image: 'http://4.bp.blogspot.com/-xsZFgAwTaow/UumqvTFjURI/AAAAAAAACGI/LdXISuoVc00/s1600/vscocam_1390962236.676736.55.jpg'
+	// }
+
+	// var facilities = [
+		
+	// 	{
+	// 		name: 'Richards Building - BYU',
+	// 		hours: '6:00 am - 9:45 pm Mon-Sat',
+	// 		phone: '(801) 422-3644',
+	// 		images: [ 
+	// 			"../assets/rb.jpg"
+	// 		]
+	// 	}
+	// 	{
+	// 		name: '24 Hour Fitness',
+	// 		hours: '6:00 am - 12:00 pm Mon-Sat',
+	// 		phone: '(801) 224-2096',
+	// 		images: [
+	// 			"../assets/24Hour.jpg"
+	// 		]
+	// 	}
+	// 	{
+	// 		name: 'Orem Recreation Center',
+	// 		hours: '5:00 am - 10:00 pm Mon-Fri
+	// 				7:00 am - 7:00 pm Sat',
+	// 		phone: '(801) 852-6600',
+	// 		images: [ 
+	// 			"../assets/OremRec.jpg"
+	// 		]
+	// 	}
+	// 	{
+	// 		name: 'Provo Recreation Center',
+	// 		hours: '5:00 am - 10:00 pm Mon-Fri
+	// 				7:00 am - 8:00 pm Sat',
+	// 		phone: '(801) 229-7154',
+	// 		images: [
+	// 			"../assets/ProvoRec.jpg" 
+	// 		]
+	// 	}
+	// ]
 
 	// var facility = {
 	// 	name: '',
