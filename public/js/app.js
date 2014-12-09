cbafc2f7d6021b7a60262c3d42b80374e690198b
Andrew <wally451@sbcglobal.net>
2014-12-08 20:05:19 -0700

    updated angular

--- public/js/app.js
+++ public/js/app.js
@@ -10,12 +10,23 @@
  		$scope.user = {
  		firstName: 'Jeff',
  		lastName: 'Stapleton',
+ 		email: 'jeff.d.stapleton@gmail.com',
+ 		phone: '(949)632-0102',
  		fbUserId: '',
  		accessToken: '',
  		image: 'http://4.bp.blogspot.com/-xsZFgAwTaow/UumqvTFjURI/AAAAAAAACGI/LdXISuoVc00/s1600/vscocam_1390962236.676736.55.jpg'
-		} 
+		};
+
+		$scope.submit = function() {
+			if($scope.user.email){
+				
+			} else {
+
+			}
+		};
+
 	});
-	
+
 })();
 
 
