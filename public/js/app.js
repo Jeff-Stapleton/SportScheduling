25078421d34522b2744864ff14b7ed7abe8d1ce1
Andrew <wally451@sbcglobal.net>
2014-12-08 18:02:53 -0700

    updated angular

--- public/js/app.js
+++ public/js/app.js
@@ -19,19 +19,17 @@
 		fbUserId: '',
 		accessToken: '',
 		image: 'http://4.bp.blogspot.com/-xsZFgAwTaow/UumqvTFjURI/AAAAAAAACGI/LdXISuoVc00/s1600/vscocam_1390962236.676736.55.jpg'
-	}; 
+		}; 
+
+		$scope.submit = function(data) {
+			
+		};
+
 	}]);
 
-	// app.controller('FacilityController', function($scope){
-	// 	$scope.facilities = facilities;
-	// });
-
-	// app.controller('FormCtrl', function($scope){
-
-	// 	$scope.submit = function(data) {
-
-	// 	};
-	// });
+	app.controller('FacilityController', ['$scope', function($scope){
+		$scope.facilities = facilities;
+	}]);
 
 	// var user = {
 	// 	firstName: 'Jeff',
@@ -43,43 +41,43 @@
 	// 	image: 'http://4.bp.blogspot.com/-xsZFgAwTaow/UumqvTFjURI/AAAAAAAACGI/LdXISuoVc00/s1600/vscocam_1390962236.676736.55.jpg'
 	// }
 
-	// var facilities = [
+	var facilities = [
 		
-	// 	{
-	// 		name: 'Richards Building - BYU',
-	// 		hours: '6:00 am - 9:45 pm Mon-Sat',
-	// 		phone: '(801) 422-3644',
-	// 		images: [ 
-	// 			"../assets/rb.jpg"
-	// 		]
-	// 	}
-	// 	{
-	// 		name: '24 Hour Fitness',
-	// 		hours: '6:00 am - 12:00 pm Mon-Sat',
-	// 		phone: '(801) 224-2096',
-	// 		images: [
-	// 			"../assets/24Hour.jpg"
-	// 		]
-	// 	}
-	// 	{
-	// 		name: 'Orem Recreation Center',
-	// 		hours: '5:00 am - 10:00 pm Mon-Fri
-	// 				7:00 am - 7:00 pm Sat',
-	// 		phone: '(801) 852-6600',
-	// 		images: [ 
-	// 			"../assets/OremRec.jpg"
-	// 		]
-	// 	}
-	// 	{
-	// 		name: 'Provo Recreation Center',
-	// 		hours: '5:00 am - 10:00 pm Mon-Fri
-	// 				7:00 am - 8:00 pm Sat',
-	// 		phone: '(801) 229-7154',
-	// 		images: [
-	// 			"../assets/ProvoRec.jpg" 
-	// 		]
-	// 	}
-	// ]
+		{
+			name: 'Richards Building - BYU',
+			hours: '6:00 am - 9:45 pm Mon-Sat',
+			phone: '(801) 422-3644',
+			images: [ 
+				"../assets/rb.jpg"
+			]
+		}
+		{
+			name: '24 Hour Fitness',
+			hours: '6:00 am - 12:00 pm Mon-Sat',
+			phone: '(801) 224-2096',
+			images: [
+				"../assets/24Hour.jpg"
+			]
+		}
+		{
+			name: 'Orem Recreation Center',
+			hours: '5:00 am - 10:00 pm Mon-Fri
+					7:00 am - 7:00 pm Sat',
+			phone: '(801) 852-6600',
+			images: [ 
+				"../assets/OremRec.jpg"
+			]
+		}
+		{
+			name: 'Provo Recreation Center',
+			hours: '5:00 am - 10:00 pm Mon-Fri
+					7:00 am - 8:00 pm Sat',
+			phone: '(801) 229-7154',
+			images: [
+				"../assets/ProvoRec.jpg" 
+			]
+		}
+	]
 
 	// var facility = {
 	// 	name: '',
