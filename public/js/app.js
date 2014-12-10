d6a68c4c78dbf1a44422d0600df692ee71d9f3db
Jeffrey D Stapleton <stapl3@anchovies.cs.byu.edu>
2014-12-10 16:44:08 -0700

    fixed syntax

--- public/js/app.js
+++ public/js/app.js
@@ -140,31 +140,31 @@
 		$scope.calendars = [
 			{
 				name: 'Sunday',
-				hours: ['7 am','8 am','9 am','10 am','11 am','12 am','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
+				hours: ['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
 			},
 			{
 				name: 'Monday',
-				hours: ['7 am','8 am','9 am','10 am','11 am','12 am','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
+				hours: ['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
 			},
 			{
 				name: 'Tuesday',
-				hours: ['7 am','8 am','9 am','10 am','11 am','12 am','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
+				hours: ['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
 			},
 			{
-				name: 'wednesday',
-				hours: ['7 am','8 am','9 am','10 am','11 am','12 am','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
+				name: 'Wednesday',
+				hours: ['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
 			},
 			{
 				name: 'Thursday',
-				hours: ['7 am','8 am','9 am','10 am','11 am','12 am','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
+				hours: ['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
 			},
 			{
 				name: 'Friday',
-				hours: ['7 am','8 am','9 am','10 am','11 am','12 am','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
+				hours: ['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
 			},
 			{
 				name: 'Saturday',
-				hours: ['7 am','8 am','9 am','10 am','11 am','12 am','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
+				hours: ['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']	
 			}
 		];
 	}]);
