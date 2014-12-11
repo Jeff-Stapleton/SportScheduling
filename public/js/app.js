a77b07fbaecca8029940651bfd3ca66104acf81f
Jeffrey D Stapleton <stapl3@skinner.cs.byu.edu>
2014-12-10 18:23:13 -0700

    editted court controller

--- public/js/app.js
+++ public/js/app.js
@@ -125,52 +125,80 @@
 				name: 'Bulldog Court',
 				image: "../assets/rb.jpg",
 				calendar: [
-				['7 am','8 am','9 am','10 am','11 am','RESERVED','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']
+				['7 am','7 am','7 am','7 am','7 am','7 am','7 am'],	
+				['8 am','8 am','8 am','8 am','8 am','8 am','8 am'],	
+				['9 am','9 am','9 am','9 am','9 am','9 am','9 am'],	
+				['10 am','10 am','10 am','10 am','10 am','10 am','10 am'],	
+				['11 am','11 am','11 am','11 am','11 am','11 am','11 am'],	
+				['12 pm','12 pm','12 pm','12 pm','12 pm','12 pm','12 pm'],	
+				['1 pm','1 pm','1 pm','1 pm','1 pm','1 pm','1 pm'],	
+				['2 pm','2 pm','2 pm','2 pm','2 pm','2 pm','2 pm'],	
+				['3 pm','3 pm','3 pm','3 pm','3 pm','3 pm','3 pm'],	
+				['4 pm','4 pm','4 pm','4 pm','4 pm','4 pm','4 pm'],	
+				['5 pm','5 pm','5 pm','5 pm','5 pm','5 pm','5 pm'],	
+				['6 pm','6 pm','6 pm','6 pm','6 pm','6 pm','6 pm'],	
+				['7 pm','7 pm','7 pm','7 pm','7 pm','7 pm','7 pm'],	
+				['8 pm','8 pm','8 pm','8 pm','8 pm','8 pm','8 pm']	
 				]				
 			},
 			{
 				name: 'Falcon Court',
 				image: "../assets/rb.jpg",
 				calendar: [
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],	
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']
+				['7 am','7 am','7 am','7 am','7 am','7 am','7 am'],	
+				['8 am','8 am','8 am','8 am','8 am','8 am','8 am'],	
+				['9 am','9 am','9 am','9 am','9 am','9 am','9 am'],	
+				['10 am','10 am','10 am','10 am','10 am','10 am','10 am'],	
+				['11 am','11 am','11 am','11 am','11 am','11 am','11 am'],	
+				['12 pm','12 pm','12 pm','12 pm','12 pm','12 pm','12 pm'],	
+				['1 pm','1 pm','1 pm','1 pm','1 pm','1 pm','1 pm'],	
+				['2 pm','2 pm','2 pm','2 pm','2 pm','2 pm','2 pm'],	
+				['3 pm','3 pm','3 pm','3 pm','3 pm','3 pm','3 pm'],	
+				['4 pm','4 pm','4 pm','4 pm','4 pm','4 pm','4 pm'],	
+				['5 pm','5 pm','5 pm','5 pm','5 pm','5 pm','5 pm'],	
+				['6 pm','6 pm','6 pm','6 pm','6 pm','6 pm','6 pm'],	
+				['7 pm','7 pm','7 pm','7 pm','7 pm','7 pm','7 pm'],	
+				['8 pm','8 pm','8 pm','8 pm','8 pm','8 pm','8 pm']	
 				]			
 			},
 			{
 				name: 'Shark Court',
 				image: "../assets/rb.jpg",
 				calendar: [
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],	
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']
+				['7 am','7 am','7 am','7 am','7 am','7 am','7 am'],	
+				['8 am','8 am','8 am','8 am','8 am','8 am','8 am'],	
+				['9 am','9 am','9 am','9 am','9 am','9 am','9 am'],	
+				['10 am','10 am','10 am','10 am','10 am','10 am','10 am'],	
+				['11 am','11 am','11 am','11 am','11 am','11 am','11 am'],	
+				['12 pm','12 pm','12 pm','12 pm','12 pm','12 pm','12 pm'],	
+				['1 pm','1 pm','1 pm','1 pm','1 pm','1 pm','1 pm'],	
+				['2 pm','2 pm','2 pm','2 pm','2 pm','2 pm','2 pm'],	
+				['3 pm','3 pm','3 pm','3 pm','3 pm','3 pm','3 pm'],	
+				['4 pm','4 pm','4 pm','4 pm','4 pm','4 pm','4 pm'],	
+				['5 pm','5 pm','5 pm','5 pm','5 pm','5 pm','5 pm'],	
+				['6 pm','6 pm','6 pm','6 pm','6 pm','6 pm','6 pm'],	
+				['7 pm','7 pm','7 pm','7 pm','7 pm','7 pm','7 pm'],	
+				['8 pm','8 pm','8 pm','8 pm','8 pm','8 pm','8 pm']	
 				]			
 			},
 			{
 				name: 'Courgar Court',
 				image: "../assets/rb.jpg",
 				calendar: [
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],	
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm'],
-				['7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm', '8 pm']
+				['7 am','7 am','7 am','7 am','7 am','7 am','7 am'],	
+				['8 am','8 am','8 am','8 am','8 am','8 am','8 am'],	
+				['9 am','9 am','9 am','9 am','9 am','9 am','9 am'],	
+				['10 am','10 am','10 am','10 am','10 am','10 am','10 am'],	
+				['11 am','11 am','11 am','11 am','11 am','11 am','11 am'],	
+				['12 pm','12 pm','12 pm','12 pm','12 pm','12 pm','12 pm'],	
+				['1 pm','1 pm','1 pm','1 pm','1 pm','1 pm','1 pm'],	
+				['2 pm','2 pm','2 pm','2 pm','2 pm','2 pm','2 pm'],	
+				['3 pm','3 pm','3 pm','3 pm','3 pm','3 pm','3 pm'],	
+				['4 pm','4 pm','4 pm','4 pm','4 pm','4 pm','4 pm'],	
+				['5 pm','5 pm','5 pm','5 pm','5 pm','5 pm','5 pm'],	
+				['6 pm','6 pm','6 pm','6 pm','6 pm','6 pm','6 pm'],	
+				['7 pm','7 pm','7 pm','7 pm','7 pm','7 pm','7 pm'],	
+				['8 pm','8 pm','8 pm','8 pm','8 pm','8 pm','8 pm']	
 				]					
 			}
 
