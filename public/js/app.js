355a066324cb74515674586dd1f6aa2807dbb6cf
Jeffrey D Stapleton <stapl3@skinner.cs.byu.edu>
2014-12-10 21:42:09 -0700

    added Modal Pop-up

--- public/js/app.js
+++ public/js/app.js
@@ -12,71 +12,7 @@
  		accessToken: '',
  		image: '',
  		loggedIn: 'false'
-		};
-
-		$scope.launch = function(which){
-		    var dlg = null;
-		    switch(which){
-		        
-		      // Error Dialog
-		      case 'error':
-		        dlg = $dialogs.error('This is my error message');
-		        break;
-		        
-		      // Wait / Progress Dialog
-		      case 'wait':
-		        dlg = $dialogs.wait(msgs[i++],progress);
-		        fakeProgress();
-		        break;
-		        
-		      // Notify Dialog
-		      case 'notify':
-		        dlg = $dialogs.notify('Something Happened!','Something happened that I need to tell you.');
-		        break;
-		        
-		      // Confirm Dialog
-		      case 'confirm':
-		        dlg = $dialogs.confirm('Please Confirm','Is this awesome or what?');
-		        dlg.result.then(function(btn){
-		          $scope.confirmed = 'You thought this quite awesome!';
-		        },function(btn){
-		          $scope.confirmed = 'Shame on you for not thinking this is awesome!';
-		        });
-		        break;
-		       
-		      // Create Your Own Dialog
-		      case 'create':
-		        dlg = $dialogs.create('../views/homePage.html.html','ModalController',{},{key: false,back: 'static'});
-		        dlg.result.then(function(name){
-		          $scope.name = name;
-		        },function(){
-		          $scope.name = 'You decided not to enter in your name, that makes me sad.';
-		        });
-		        
-		        break;
-		    }; // end switch
-		}; // end launch
-  
-  		app.controller('ModalController',function($scope,$modalInstance,data){
-			$scope.user = {name : ''};
-
-			$scope.cancel = function(){
-				$modalInstance.dismiss('canceled');  
-			  	}; // end cancel
-			  
-			$scope.save = function(){
-			    $modalInstance.close($scope.user.name);
-			 	 }; // end save
-			  
-			$scope.hitEnter = function(evt){
-			    if(angular.equals(evt.keyCode,13) && !(angular.equals($scope.name,null) || angular.equals($scope.name,'')))
-					$scope.save();
-			  	}; // end hitEnter
-			}) // end whatsYourNameCtrl
-			app.run(['$templateCache',function($templateCache){
-			  	$templateCache.put('../views/homePage.html','<div class="modal"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title"><span class="glyphicon glyphicon-star"></span> User\'s Name</h4></div><div class="modal-body"><ng-form name="nameDialog" novalidate role="form"><div class="form-group input-group-lg" ng-class="{true: \'has-error\'}[nameDialog.username.$dirty && nameDialog.username.$invalid]"><label class="control-label" for="username">Name:</label><input type="text" class="form-control" name="username" id="username" ng-model="user.name" ng-keyup="hitEnter($event)" required><span class="help-block">Enter your full name, first &amp; last.</span></div></ng-form></div><div class="modal-footer"><button type="button" class="btn btn-default" ng-click="cancel()">Cancel</button><button type="button" class="btn btn-primary" ng-click="save()" ng-disabled="(nameDialog.$dirty && nameDialog.$invalid) || nameDialog.$pristine">Save</button></div></div></div></div>');
-			}]); // end run / module
-
+		}; 		
 
 		$scope.submit = function(phone) {
 		 	$scope.user.phone = phone;
@@ -264,28 +200,5 @@
 			];
 
 		}]);
-
-	app.controller('ModalController',function($scope,$modalInstance,data){
-			$scope.user = {name : ''};
-
-			$scope.cancel = function(){
-				$modalInstance.dismiss('canceled');  
-			  	}; // end cancel
-			  
-			$scope.save = function(){
-			    $modalInstance.close($scope.user.name);
-			 	 }; // end save
-			  
-			$scope.hitEnter = function(evt){
-			    if(angular.equals(evt.keyCode,13) && !(angular.equals($scope.name,null) || angular.equals($scope.name,'')))
-					$scope.save();
-			  	}; // end hitEnter
-		}) // end whatsYourNameCtrl
-	
-		app.run(['$templateCache',function($templateCache){
-		  	$templateCache.put('../views/homePage.html','<div class="modal"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title"><span class="glyphicon glyphicon-star"></span> User\'s Name</h4></div><div class="modal-body"><ng-form name="nameDialog" novalidate role="form"><div class="form-group input-group-lg" ng-class="{true: \'has-error\'}[nameDialog.username.$dirty && nameDialog.username.$invalid]"><label class="control-label" for="username">Name:</label><input type="text" class="form-control" name="username" id="username" ng-model="user.name" ng-keyup="hitEnter($event)" required><span class="help-block">Enter your full name, first &amp; last.</span></div></ng-form></div><div class="modal-footer"><button type="button" class="btn btn-default" ng-click="cancel()">Cancel</button><button type="button" class="btn btn-primary" ng-click="save()" ng-disabled="(nameDialog.$dirty && nameDialog.$invalid) || nameDialog.$pristine">Save</button></div></div></div></div>');
-		}]); // end run / module
-	
-
 
 })();
